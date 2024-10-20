bd33b488a11f5d6a56f076a1976516b456723d15
Admin <admin@example.com>
2024-10-20 18:15:56 +0700

    them cái test

---
+++ test.py
@@ -0,0 +1,13 @@
+POST /bookdb_index/book/_search
+{
+    "query": {
+        "multi_match" : {
+            "query" : "elasticsearch guide",
+            "fields": ["title", "summary"]
+        }
+    }
+}
+#Thêm xem sao
+
+
+#okie
