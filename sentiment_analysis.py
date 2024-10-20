2c1c0e8857bec29cafe3d80302f21ee373f898a5
Admin <admin@example.com>
2024-10-20 16:56:50 +0700

    Cập nhật file sentiment_analysis.py

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -6,3 +6,6 @@ classifier = pipeline("sentiment-analysis")
 # Phân loại một câu
 result = classifier("Hôm nay là một ngày tuyệt vời!")
 print(result)  # Kết quả sẽ cho bạn biết cảm xúc của câu
+
+# thêm mới cho oách
+
